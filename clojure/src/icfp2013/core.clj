be8e2b735cb428abdb5d2197e27ec4f09c9c6610
Alexander Kiel <alexanderkiel@gmx.net>
2013-08-09 16:38:23 +0200

    eval works with some tests

--- clojure/src/icfp2013/core.clj
+++ clojure/src/icfp2013/core.clj
@@ -1,9 +1,13 @@
 (ns icfp2013.core
   (:require [clojure.tools.namespace.repl :refer (refresh)]
             [clojure.pprint :refer (pprint)]
-            [icfp2013.problems :as problems]))
+            [icfp2013.util :refer (hex)]
+            [icfp2013.problems :as problems]
+            [icfp2013.eval :as eval :refer (not shl1 shr1 shr4 shr16
+                                             or and xor plus if0 lambda fold)])
+  (:refer-clojure :exclude [not or and]))
 
-(println
+(comment
   "Use (refresh) to refresh the project.
 
-Use (pprint (problems/w-size 3)) to list all problems with size 3.")
+   Use (pprint (problems/w-size 3)) to list all problems with size 3.")
--- clojure/src/icfp2013/problems.clj
+++ clojure/src/icfp2013/problems.clj
@@ -1,8 +1,6 @@
 (ns icfp2013.problems
   (:require [clj-http.client :as c]
-            [clojure.data.json :as json]
-            [clojure.tools.namespace.repl :refer (refresh)]
-            [clojure.pprint :refer (pprint)]))
+            [clojure.data.json :as json]))
 
 (def uri "http://icfpc2013.cloudapp.net/myproblems?auth=0346eBiCRDFzfUqcEJSHQrAM2MvLPojl7V373Vs8vpsH1H")
 
