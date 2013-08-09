6a40c1a7ea51c8b699d156adf97f9aa549a1a483
Alexander Kiel <alexanderkiel@gmx.net>
2013-08-09 12:53:38 +0200

    initial version of problems namespace

---
+++ clojure/src/problems/core.clj
@@ -0,0 +1,18 @@
+(ns problems.core
+  (:require [clj-http.client :as c]
+            [clojure.data.json :as json]
+            [clojure.tools.namespace.repl :refer (refresh)]
+            [clojure.pprint :refer (pprint)]))
+
+(def uri "http://icfpc2013.cloudapp.net/myproblems?auth=0346eBiCRDFzfUqcEJSHQrAM2MvLPojl7V373Vs8vpsH1H")
+
+(defn fetch-problems []
+  (-> uri
+    c/get
+    :body
+    (json/read-str :key-fn keyword)))
+
+(def problems (memoize fetch-problems))
+
+(defn w-size [size]
+  (->> (problems) (filter #(= (:size %1) size))))
