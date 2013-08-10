d360ccbe33c76e2e66aef1a3ee464e4b283d6264
Alexander Kiel <alexanderkiel@gmx.net>
2013-08-10 16:56:50 +0200

    added all-wasted

--- clojure/src/icfp2013/problems.clj
+++ clojure/src/icfp2013/problems.clj
@@ -3,10 +3,19 @@
 
 (def problems c/problems)
 
-(defn open? [problem]
-  (not (:solved problem)))
+(defn open? [{:keys [solved timeLeft]
+              :or {timeLeft 300}}]
+  (and (not solved) (< 0 timeLeft)))
+
+(defn wasted? [{:keys [solved timeLeft]}]
+  (and (not solved) (= 0 timeLeft)))
 
 (defn w-size [size]
   (->> (problems)
     (filter open?)
-    (filter #(= (:size %1) size))))
+    (filter #(= (:size %1) size))
+    (sort-by :id)))
+
+(defn all-wasted []
+  (->> (problems)
+    (filter wasted?)))
