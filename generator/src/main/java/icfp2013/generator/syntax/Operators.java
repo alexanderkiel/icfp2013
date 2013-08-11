de82d6258a62a9f4cd2c2fdaa24c3b302905b590
knoppiks <knoppizillin@gmx.de>
2013-08-11 13:04:33 +0200

    [fix] fixed outOfBounds- and other -exceptions

--- generator/src/main/java/icfp2013/generator/syntax/Operators.java
+++ generator/src/main/java/icfp2013/generator/syntax/Operators.java
@@ -4,16 +4,29 @@ package icfp2013.generator.syntax;
  * @author <a href="mailto:jonas.wagner@life.uni-leipzig.de">Jonas Wagner</a>
  */
 public enum Operators {
-    NOT(1, 2), SHL1(1, 2), SHR1(1, 2), SHR4(1, 2), SHR16(1, 2),
-    AND(2, 3), OR(2, 3), XOR(2, 3), PLUS(2, 3),
-    IF0(1, 4),
-    FOLD(2, 5), TFOLD(2, 5);
+    NOT(1, 2, 1),
+    SHL1(1, 2, 1),
+    SHR1(1, 2, 1),
+    SHR4(1, 2, 1),
+    SHR16(1, 2, 1),
+
+    AND(2, 3, 2),
+    OR(2, 3, 2),
+    XOR(2, 3, 2),
+    PLUS(2, 3, 2),
+
+    IF0(1, 4, 3),
+
+    FOLD(2, 5, 4),
+    TFOLD(2, 5, 1);
 
     public final int operatorSize;
     public final int minimumSize;
+    public final int numArgs;
 
-    Operators(int operatorSize, int minimumSize) {
+    Operators(int operatorSize, int minimumSize, int numArgs) {
         this.operatorSize = operatorSize;
         this.minimumSize = minimumSize;
+        this.numArgs = numArgs;
     }
 }
--- generator/src/test/java/icfp2013/generator/ProgramGeneratorTest.java
+++ generator/src/test/java/icfp2013/generator/ProgramGeneratorTest.java
@@ -1,6 +1,7 @@
 package icfp2013.generator;
 
 import icfp2013.generator.syntax.Program;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Random;
@@ -12,9 +13,14 @@ import static icfp2013.generator.syntax.Operators.IF0;
 import static icfp2013.generator.syntax.Operators.NOT;
 import static icfp2013.generator.syntax.Operators.OR;
 import static icfp2013.generator.syntax.Operators.PLUS;
+import static icfp2013.generator.syntax.Operators.SHL1;
 import static icfp2013.generator.syntax.Operators.SHR1;
+import static icfp2013.generator.syntax.Operators.SHR16;
 import static icfp2013.generator.syntax.Operators.SHR4;
+import static icfp2013.generator.syntax.Operators.XOR;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author <a href="mailto:jonas.wagner@life.uni-leipzig.de">Jonas Wagner</a>
@@ -57,19 +63,26 @@ public class ProgramGeneratorTest {
 
     @Test
     public void testOnlyIf() throws Exception {
-        int size = 100, min = 9999999, max = -1;
+        int size = 100;
         ProgramGenerator generator = new ProgramGenerator(newArrayList(IF0), size);
 
-        for (int i = 0; i < 1000; i++) {
-            Program p = generator.apply(new Random(i * i));
-            int size1 = p.size();
-            min = Math.min(min, size1);
-            max = Math.max(max, size1);
+        Program program = generator.apply(new Random());
 
+        assertEquals(size, program.size());
+        System.out.println(program);
+    }
+
+    @Ignore("is unlikely ever to succeed without a really heavy algorithm")
+    @Test
+    public void testNoProgramEquals() {
+        int size = 30;
+        ProgramGenerator generator =
+                new ProgramGenerator(newArrayList(NOT, SHL1, SHR1, SHR4, SHR16, AND, OR, XOR, PLUS, IF0, FOLD), size);
+
+        Program program = generator.apply(new Random());
+        for (int i = 0; i < 30; i++) {
+            assertNotEquals(program, generator.apply(new Random()));
         }
-
-        System.out.println("min: " + min);
-        System.out.println("max: " + max);
-//        assertEquals(size, p.size());
+        assertTrue(true);
     }
 }
