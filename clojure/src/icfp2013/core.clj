bae9c003f9230e67de4417a7e3ce6024ae609a19
Alexander Kiel <alexanderkiel@gmx.net>
2013-08-11 12:22:52 +0200

    solve-sa works for some problems with size 8

--- clojure/src/icfp2013/core.clj
+++ clojure/src/icfp2013/core.clj
@@ -3,7 +3,8 @@
             [clojure.string :as str]
             [clojure.pprint :refer (pprint)]
             [clojure.repl :refer (pst)]
-            [clojure.walk :refer (prewalk postwalk)]
+            [clojure.set :refer (union)]
+            [clojure.walk :refer (walk prewalk postwalk)]
             [icfp2013.util :refer (to-hex to-bin from-hex gen-inputs)]
             [icfp2013.client :as c]
             [icfp2013.problems :as problems]
@@ -45,6 +46,25 @@
 (def op2s ['and 'or 'xor 'plus])
 (def op2? (apply hash-set op2s))
 
+(defn lambda? [node]
+  (clojure.core/and (list? node) (= 'lambda (first node))))
+
+(defn arg-list?
+  "Returns true iff the node is a lambda arg list."
+  [node]
+  (clojure.core/and
+    (list? node)
+    (clojure.core/not (clojure.core/or
+                        (op1? (first node))
+                        (op2? (first node))
+                        (= 'lambda (first node))))))
+
+(defn op2-node? [node]
+  (clojure.core/and
+    (list? node)
+    (= 3 (count node))
+    (clojure.core/not (lambda? node))))
+
 (defn mutate-op1 [op]
   (if (< (rand) 0.3)
     (rand-nth op1s)
@@ -55,28 +75,45 @@
     (rand-nth op2s)
     op))
 
-(defn mutate-const [c]
+(defn mutate-const [vars c]
   (if (< (rand) 0.4)
-    (if (= 0 c) 1 0)
+    (rand-nth (conj (vec vars) 0 1))
     c))
 
-(defn mutate-list [n]
-  (if (= 'lambda (first n))
-    n
-    (if (clojure.core/and (= 3 (count n)) (< (rand) 0.2))
-      (list (first n) (nth n 2) (nth n 1))
-      n)))
+(defn mutate-var [vars v]
+  (if (< (rand) 0.2)
+    (rand-nth (conj (vec vars) 0 1))
+    v))
 
-(defn mutate-node [n]
+(defn mutate-list [n]
+  (if (clojure.core/and (< (rand) 0.2) (op2-node? n))
+    (list (first n) (nth n 2) (nth n 1))
+    n))
+
+(defn mutate-node [vars n]
   (cond
     (list? n) (mutate-list n)
     (op1? n) (mutate-op1 n)
     (op2? n) (mutate-op2 n)
-    (#{0 1} n) (mutate-const n)
+    (#{0 1} n) (mutate-const vars n)
+    (not= 'lambda n) (mutate-var vars n)
     :else n))
 
-(defn neighbour [s]
-  (prewalk mutate-node s))
+(defn find-vars
+  "Returns a set of all vars defined in node. Only lambda nodes define vars."
+  [node]
+  (if (lambda? node)
+    (set (second node))
+    (hash-set)))
+
+(defn neighbour
+  "Returns a neighbour of prog possibly using vars."
+  ([prog] (neighbour (hash-set) prog))
+  ([vars prog]
+;    (println {:vars vars :prog prog})
+    (let [descend (partial neighbour (union vars (find-vars prog)))
+          inner (if (arg-list? prog) identity descend)]
+      (walk inner identity (mutate-node vars prog)))))
 
 (defn p [e e1 t]
   (if (< e1 e)
@@ -86,23 +123,25 @@
 (defn sa [operators size inputs outputs]
   (loop [s (p/read (p/gen (vec operators) size))
          e (energy s inputs outputs)
-         t 10000.0]
-    (if (= e 0.0)
-      s
+         t 0.1]
+    (if (clojure.core/or (= e 0.0) (< t 0.001))
+      [s e]
       (let [s1 (neighbour s)
             e1 (energy s1 inputs outputs)
             p (p e e1 t)]
-        (printf "e: %1.4f t: %3.2f p: %1.2f s: %s%n" e t p s)
+        (printf "e: %1.4f e1: %1.4f t: %3.2f p: %1.2f s: %s%n" e e1 t p s)
         (if (> p (rand))
-          (recur s1 e1 (* t 0.99))
-          (recur s e (* t 0.99)))))))
+          (recur s1 e1 (* t 0.999))
+          (recur s e (* t 0.999)))))))
 
 (defn solve-sa
   "Simulated annealing solver."
   [id size operators inputs outputs]
-  (let [prog (sa operators size inputs outputs)]
+  (loop [[prog e] (sa operators size inputs outputs)]
     (println "prog:" prog)
-    (c/guess id (pr-str prog))))
+    (if (= 0.0 e)
+      (c/guess id (pr-str prog))
+      (recur (sa operators size inputs outputs)))))
 
 (defn solve [{:keys [id size operators]} solver inputs]
   (let [operators (->> operators (map (partial symbol "p")) (map eval))
